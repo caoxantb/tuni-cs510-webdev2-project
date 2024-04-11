6c597b4e86d50eda4980b9b82161b8581f43525c
Xuan-An Cao <an.cao@tuni.fi>
2024-04-11 03:51:16 +0300

    feat: small update for reset-db

--- backend/server-a/db/reset-db.js
+++ backend/server-a/db/reset-db.js
@@ -1,4 +1,7 @@
+//NOTE: This file is only for development purposes and should not be ran under any production circumstances.
+
 import { readFile } from "fs/promises";
+import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -15,14 +18,29 @@ const readJsonFile = async (path) => {
   try {
     const sandwiches = await readJsonFile("./json/sandwiches.json");
     const toppings = await readJsonFile("./json/toppings.json");
+    const users = await readJsonFile("./json/users.json");
+
+    const sandwichesUpdated = sandwiches.map((sandwiches) => ({
+      ...sandwiches,
+      breadType: ["oat", "rye", "wheat"][Math.floor(Math.random() * 3)],
+    }));
+
+    const usersAuthenticated = await Promise.all(
+      users.map(async (user) => {
+        const { password, ...userRest } = user;
+        const passwordHash = await bcrypt.hash(password, 10);
+        return { ...userRest, passwordHash };
+      })
+    );
 
     await Sandwich.deleteMany();
     await Topping.deleteMany();
     await User.deleteMany();
     await Order.deleteMany();
 
-    await Sandwich.create(sandwiches);
+    await Sandwich.create(sandwichesUpdated);
     await Topping.create(toppings);
+    await User.create(usersAuthenticated);
   } catch (err) {
     console.error(err);
   } finally {
